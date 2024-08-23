ae4ed6669466c2b3626cebb428c77854bb4a9b20
williford-abunas <abunaswilliford@gmail.com>
2024-08-24 01:56:33 +1200

    Test API query params

--- tests/Api testing/api-test.spec.ts
+++ tests/Api testing/api-test.spec.ts
@@ -2,13 +2,22 @@ import { test, expect, request, APIRequestContext } from '@playwright/test'
 
 test.describe('Api Testing with Playwright', () => {
   let apiContext: APIRequestContext
+  let orderId: number
+  const token =
+    'b81998b8a28636557d6f06db3bd7ebbc2096351ff4dcf85d8158aa2f2d1a15c0'
+
+  type Book = {
+    id: number
+    name: string
+    type: string
+    available: boolean
+  }
 
   test.beforeAll(async ({ playwright }) => {
     apiContext = await request.newContext({
       baseURL: 'https://simple-books-api.glitch.me',
       extraHTTPHeaders: {
-        Authorization:
-          'bf438d7bc080dd5b6a7187430eef29fd8aad09d8b135631d20ae6575c210bd9b',
+        Authorization: `Bearer ${token}`,
       },
     })
   })
@@ -42,5 +51,49 @@ test.describe('Api Testing with Playwright', () => {
       expect(typeof firstBook.type).toBe('string')
       expect(typeof firstBook.available).toBe('boolean')
     }
+  })
+
+  test('Get only 2 fiction type books', async () => {
+    const response = await apiContext.get('/books', {
+      params: {
+        type: 'fiction',
+        limit: 2,
+      },
+    })
+
+    expect(response.ok()).toBeTruthy()
+    const data = await response.json()
+    // Check data is an array
+    expect(Array.isArray(data)).toBeTruthy()
+    // Check there's only 2 items
+    expect(data.length).toBe(2)
+
+    data.forEach((book: Book) => {
+      expect(book).toHaveProperty('id')
+      expect(book).toHaveProperty('name')
+      expect(book).toHaveProperty('type')
+      expect(book).toHaveProperty('available')
+
+      // Type checks
+      expect(typeof book.id).toBe('number')
+      expect(typeof book.name).toBe('string')
+      expect(typeof book.type).toBe('string')
+      expect(book.type).toBe('fiction') // Ensure the type is "fiction"
+      expect(typeof book.available).toBe('boolean')
+    })
+  })
+
+  test('Submit a new order', async () => {
+    const response = await apiContext.post('/orders', {
+      data: {
+        bookId: 1,
+        customerName: 'Will Abunas',
+      },
+    })
+
+    expect(response.status()).toBe(201)
+    const body = await response.json()
+    orderId = body.orderId
+    expect(orderId).toBeTruthy()
   })
 })
