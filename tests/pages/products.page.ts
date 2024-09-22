29f6185d9c28c81f2b955ecd89d73d844c7e7a15
williford-abunas <abunaswilliford@gmail.com>
2024-09-23 02:05:16 +1200

    remove logs/unused

--- tests/pages/products.page.ts
+++ tests/pages/products.page.ts
@@ -7,23 +7,18 @@ const baseURL = process.env.BASE_URL
 console.log(baseURL)
 
 export class ProductsPage {
-  // Variable declarations
   readonly page: Page
   readonly addToCartButtons: Locator
   readonly closeCheckoutButton: Locator
   readonly cartQuantity: Locator
   readonly productPriceText: Locator
   readonly cartButton: Locator
-  readonly productCountText: Locator
   readonly checkoutButton: Locator
   readonly sizeFilter: (size: string) => Locator
-  readonly subtractButton: Locator
-  readonly addButton: Locator
   readonly removeButton: Locator
   readonly totalPriceText: Locator
   readonly quantityElement: Locator
 
-  // Constructor to initialize the page object with a Playwright page ===============
   constructor(page: Page) {
     this.page = page
     this.addToCartButtons = page.getByRole('button', { name: 'Add to cart' })
@@ -31,9 +26,6 @@ export class ProductsPage {
     this.checkoutButton = page.locator('button:has-text("Checkout")')
     this.cartQuantity = page.locator('.sc-1h98xa9-3')
     this.productPriceText = page.locator('.sc-11uohgb-4 p')
-    this.productCountText = page.locator('text=/\\d+ Product\\(s\\) found/')
-    this.subtractButton = page.getByRole('button', { name: '-', exact: true })
-    this.addButton = page.getByRole('button', { name: '+', exact: true })
     this.sizeFilter = (size: string) =>
       page.getByText(`${size}`, { exact: true })
     this.removeButton = page.locator('button[title="remove product from cart"]')
@@ -43,8 +35,6 @@ export class ProductsPage {
     this.totalPriceText = page.locator('p.sc-1h98xa9-9.jzywDV')
     this.quantityElement = page.locator('p.sc-11uohgb-3.gKtloF')
   }
-
-  // Methods ==========================================================================
 
   async navigateToShop() {
     if (!baseURL) {
@@ -102,17 +92,15 @@ export class ProductsPage {
   }
 
   async removeMultipleItems(count: number) {
-    let itemsCount = await this.removeButton.count() // Count the total number of remove buttons
+    let itemsCount = await this.removeButton.count()
 
-    // Loop over the items and remove them one by one
     for (let i = 0; i < Math.min(count, itemsCount); i++) {
       console.log(`Removing item ${i + 1} from the cart`)
 
-      // Wait for the remove button to be visible and clickable
-      const currentRemoveButton = this.removeButton.nth(0) // Always target the first button since the DOM is dynamic
+      const currentRemoveButton = this.removeButton.nth(0)
 
-      await currentRemoveButton.waitFor({ state: 'visible' }) // Ensure the button is visible
-      await currentRemoveButton.click() // Click the button to remove the item
+      await currentRemoveButton.waitFor({ state: 'visible' })
+      await currentRemoveButton.click()
     }
   }
 
@@ -120,11 +108,9 @@ export class ProductsPage {
     const allPriceTexts = await this.productPriceText.allTextContents()
     console.log(allPriceTexts)
 
-    // Use reduce to sum the prices
     const total = allPriceTexts.reduce((acc, price) => {
       const numericPrice = this.convertPriceStringToNumber(price)
 
-      // Add to the accumulator
       return acc + numericPrice
     }, 0)
 
@@ -136,12 +122,10 @@ export class ProductsPage {
     return this.convertPriceStringToNumber(totalPrice)
   }
 
-  // Helper to convert price string to number
   convertPriceStringToNumber(priceText: string | null): number {
     if (!priceText) {
       return 0
     }
-    // Remove dollar sign, trim spaces, then convert to number
     return parseFloat(priceText.replace('$', '').trim())
   }
 
